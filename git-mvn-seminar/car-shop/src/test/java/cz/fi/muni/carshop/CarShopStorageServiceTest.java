407ef9ce2a7c5aee816c084dbfcee553bd9ebbd1
bencikpeter <bencik.peter.ml@gmail.com>
2017-10-24 14:05:17 +0200

    completed

--- git-mvn-seminar/car-shop/src/test/java/cz/fi/muni/carshop/CarShopStorageServiceTest.java
+++ git-mvn-seminar/car-shop/src/test/java/cz/fi/muni/carshop/CarShopStorageServiceTest.java
@@ -1,5 +1,6 @@
 package cz.fi.muni.carshop;
 
+import com.sun.corba.se.impl.protocol.RequestCanceledException;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
@@ -16,8 +17,11 @@ import org.junit.Rule;
 
 import cz.fi.muni.carshop.entities.Car;
 import cz.fi.muni.carshop.enums.CarTypes;
+import cz.fi.muni.carshop.exceptions.RequestedCarNotFoundException;
 import cz.fi.muni.carshop.services.CarShopStorageService;
 import cz.fi.muni.carshop.services.CarShopStorageServiceImpl;
+import java.util.Optional;
+import org.junit.Assert;
 
 public class CarShopStorageServiceTest {
 
@@ -51,7 +55,7 @@ public class CarShopStorageServiceTest {
 
 		assertThat(cars, hasKey(CarTypes.AUDI));
 	}
-
+        
 	// expected to fail with JUnit < 4.11
 	@Test
 	public void testGetCheaperCars_returnsCorrectResult() {
@@ -59,10 +63,38 @@ public class CarShopStorageServiceTest {
 		service.addCarToStorage(new Car(Color.BLACK, CarTypes.AUDI, 2016, 889000));
 		service.addCarToStorage(new Car(Color.WHITE, CarTypes.AUDI, 2016, 859000));
 		service.addCarToStorage(new Car(Color.BLUE, CarTypes.AUDI, 2016, 909000));
+                service.addCarToStorage(new Car(Color.BLUE, CarTypes.AUDI, 2014, 909000));
 
 		assertThat(service.getCheaperCarsOfSameTypeAndYear(new Car(Color.BLACK, CarTypes.AUDI, 2016, 900000)),
 				hasSize(3));
 
 	}
+        
+        
+        @Test
+        public void testSellCar_OK(){
+            
+            Optional<Car> cars = service.isCarAvailable(Color.BLACK, CarTypes.AUDI);
+            
+            Car car  = new Car(Color.BLACK, CarTypes.AUDI, 2016, 899000);
+            service.addCarToStorage(car);
+            
+            try {
+                service.sellCar(car);
+            } catch (RequestedCarNotFoundException ex) {
+                Assert.fail();
+            }
+            
+            assertTrue(service.isCarAvailable(Color.BLACK, CarTypes.AUDI).equals(cars));
+        }
+        
+        @Test
+        public void testSellCar_NOK() throws RequestedCarNotFoundException {
+            thrown.reportMissingExceptionWithMessage("We expect RequestedCarNotFound exception")
+                    .expect(RequestedCarNotFoundException.class);
+            
+            Car car  = new Car(Color.BLACK, CarTypes.AUDI, 2014, 899000);
+            service.sellCar(car);
+        }
 
 }
